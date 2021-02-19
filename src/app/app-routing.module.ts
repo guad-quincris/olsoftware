065e246ac47c54b2cbcb8fe9c06083c876c8d3eb
guad-quincris <guad.quincris@hotmail.com>
2021-02-19 17:19:00 -0500

    implementation of routs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { WebappComponent } from './webapp/webapp.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "/welcome", pathMatch: "full" },
+  { path: "welcome", component: HomeComponent },
+  { path: "web", component: WebappComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
